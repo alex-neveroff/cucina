2923ac0c939daaf1f7820e052abf7b4ec117873d
Alexandr Neveroff <neveroff1976@gmail.com>
2022-12-30 21:54:48 +0200

    Hero - mobile

---
+++ src/index.js
@@ -0,0 +1,17 @@
+$('.slider-products').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  arrows: true,
+  fade: false,
+  dots: false,
+});
+
+$('.slider-gallery').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
