4f047068346c1571afd5e32df3ca2115b991b6ea
alex-neveroff <114777678+alex-neveroff@users.noreply.github.com>
2023-01-01 20:27:04 +0000

    Deploying to gh-pages from @ alex-neveroff/cucina@943f7b1df14f891fa603768b24aa1977e2a7e095 🚀

--- index.74dddfd1.js
+++
@@ -1,2 +0,0 @@
-$(".slider-products").slick({slidesToShow:2,slidesToScroll:2,arrows:!0,fade:!1,dots:!1}),$(".slider-gallery").slick({slidesToShow:1,slidesToScroll:1,arrows:!1,fade:!0,dots:!0,autoplay:!0,autoplaySpeed:4e3});
-//# sourceMappingURL=index.74dddfd1.js.map
