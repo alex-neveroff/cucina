943f7b1df14f891fa603768b24aa1977e2a7e095
Alexandr Neveroff <neveroff1976@gmail.com>
2023-01-01 22:26:32 +0200

    header - mob, tab, desk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[menu-open]'),
+    closeMenuBtn: document.querySelector('[menu-close]'),
+    menu: document.querySelector('[menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
 $('.slider-products').slick({
   slidesToShow: 2,
   slidesToScroll: 2,
